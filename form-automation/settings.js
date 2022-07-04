21410bf86ac0eb7d06b2eb8c4fcaf657f8997c71
Bruno Prado <bwprado@gmail.com>
2022-07-04 13:33:58 -0300

    fix: change email ID.

--- form-automation/settings.js
+++ form-automation/settings.js
@@ -7,15 +7,15 @@ export const campuses = {
 };
 
 export const departments = {
-  childrens: "3bd7e235-4bac-4f38-8548-cfee288352c9",
+  children: "3bd7e235-4bac-4f38-8548-cfee288352c9",
   youth: "4de4bd29-8104-41bd-a6fd-5a9a810a3d63",
   latino: "c358f96b-a408-4abb-bf2f-66814e96728e",
   chinese: "987a0493-7b4a-465d-bd63-cdfe919c7ba4",
   online: "b0d1d592-0edf-4e74-bcf0-99f4d445fb43",
 };
 
-export const PLANNED_VISIT_EMAIL_ID = "TANQHBQ";
-export const CHILDREN_EMAIL_ID = "3bd7e235-4bac-4f38-8548-cfee288352c9";
+export const STAFF_EMAIL_ID = "TANQHBQ";
+export const VISITOR_EMAIL_ID = "TAdIaCc";
 
 export const campusesEmailsIds = {
   "Form - Visit Online Campus": "b0d1d592-0edf-4e74-bcf0-99f4d445fb43",
