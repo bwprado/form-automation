22367e3ad1cdbc62e7e273add6a2ebadb64b80ee
Bruno Prado <bwprado@gmail.com>
2023-01-19 23:05:05 -0300

    🚧 Tried using `http-functions` to do the job, but it did not work

--- rss-feed/http-functions.js
+++ rss-feed/http-functions.js
@@ -1,18 +1,91 @@
 import wixData from "wix-data"
+import { Buffer } from "buffer"
+import { redirect } from "wix-router"
+import { mediaManager } from "wix-media-backend"
 import { ok, notFound, serverError, badRequest } from "wix-http-functions"
 import { getYear, getMonth, getDate, getHours, getMinutes } from "date-fns"
+
+export async function getEventsxml() {
+  const ics = require("ics")
+  let options = {
+    headers: {
+      "Content-Type": "text/document"
+    }
+  }
+
+  try {
+    const { items } = await wixData
+      .query("Events")
+      .ge("eventEndDate", new Date())
+      .ne("eventIsHidden", true)
+      .ascending("eventStartDate")
+      .find()
+
+    const formattedItems = items.map(
+      ({
+        eventTitle,
+        eventDescription,
+        eventStartDate,
+        eventLocationAddress,
+        eventRegistrationUrl
+      }) => ({
+        start: [
+          getYear(eventStartDate),
+          getMonth(eventStartDate) + 1,
+          getDate(eventStartDate),
+          getHours(eventStartDate),
+          getMinutes(eventStartDate)
+        ],
+        title: eventTitle,
+        description: eventDescription,
+        location: eventLocationAddress,
+        url: eventRegistrationUrl
+      })
+    )
+
+    const { error, value } = ics.createEvents(formattedItems)
+    if (error) return console.log(error)
+
+    options.body = items.length > 0 ? { value } : { error: "No events found" }
+
+    const buffer = Buffer.from(value)
+    const icsFile = await mediaManager.upload(
+      "iCalendar",
+      buffer,
+      "events.ics",
+      {
+        mediaOptions: {
+          mimeType: "text/calendar",
+          mediaType: "document"
+        },
+        metadataOptions: {
+          isPrivate: false,
+          isVisitorUpload: true
+        }
+      }
+    )
+    const wixUrl = icsFile?.fileUrl
+
+    if (!wixUrl) return console.log("No file URL returned")
+
+    const downloadUrl = await mediaManager.getFileUrl(wixUrl)
+    return redirect(downloadUrl, "200")
+  } catch (error) {
+    return console.log(error)
+  }
+}
 
 /**
  * @author Threed Software
  * @description Returns a list of events in ICS format string from the Events collection
- * @param {$w.} request
- * @returns {Promise<String>}
+ * @param {any} request
+ * @returns {Promise<any>}
  */
 export async function get_eventsxml(request) {
   const ics = require("ics")
   let options = {
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "text/document"
     }
   }
 
@@ -58,57 +131,29 @@ export async function get_eventsxml(request) {
 
     options.body = items.length > 0 ? { value } : { error: "No events found" }
 
-    return items.length > 0 ? ok(options) : notFound(options)
+    const buffer = Buffer.from(value)
+    const icsFile = await mediaManager.upload(
+      "iCalendar",
+      buffer,
+      "events.ics",
+      {
+        mediaOptions: {
+          mimeType: "text/calendar",
+          mediaType: "document"
+        },
+        metadataOptions: {
+          isPrivate: false,
+          isVisitorUpload: true
+        }
+      }
+    )
+    const wixUrl = icsFile?.fileUrl
+
+    if (!wixUrl) return serverError("No file URL returned")
+
+    const downloadUrl = await mediaManager.getFileUrl(wixUrl)
+    return redirect(downloadUrl, "200")
   } catch (error) {
     return serverError(error)
-  }
-}
-
-export async function getEventsxml() {
-  const ics = require("ics")
-  let options = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }
-
-  try {
-    const { items } = await wixData
-      .query("Events")
-      .ge("eventEndDate", new Date())
-      .ne("eventIsHidden", true)
-      .ascending("eventStartDate")
-      .find()
-
-    const formattedItems = items.map(
-      ({
-        eventTitle,
-        eventDescription,
-        eventStartDate,
-        eventLocationAddress,
-        eventRegistrationUrl
-      }) => ({
-        start: [
-          getYear(eventStartDate),
-          getMonth(eventStartDate) + 1,
-          getDate(eventStartDate),
-          getHours(eventStartDate),
-          getMinutes(eventStartDate)
-        ],
-        title: eventTitle,
-        description: eventDescription,
-        location: eventLocationAddress,
-        url: eventRegistrationUrl
-      })
-    )
-
-    const { error, value } = ics.createEvents(formattedItems)
-    if (error) return console.log(error)
-
-    options.body = items.length > 0 ? { value } : { error: "No events found" }
-
-    return items.length > 0 && value
-  } catch (error) {
-    return console.log(error)
   }
 }
