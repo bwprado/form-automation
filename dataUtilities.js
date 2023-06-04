1b683110352fbf07bdb9a490ba0adc3d4addc799
Bruno Prado <bwprado@gmail.com>
2023-06-04 05:55:10 -0300

    🎨 Prettier formatting

--- dataUtilities.js
+++ dataUtilities.js
@@ -3,54 +3,63 @@
 /**
  * @fileoverview Utiltiies for Data Functions
  */
-import wixData from 'wix-data';
-import {fetch} from 'wix-fetch';
+import wixData from 'wix-data'
+import { fetch } from 'wix-fetch'
 
 // Code written in public files is shared by your site's
 // Backend, page code, and site code environments.
 
 // getAllData retrieves all data using the hasNext and Next capabilities
 export async function getAllData(dataCollection) {
-	let results = await wixData.query('Ministries').include('serviceOpportunities').find();
-    let length = results.length
-    let items = length ? results.items : [];
+  let results = await wixData
+    .query('Ministries')
+    .include('serviceOpportunities')
+    .find()
+  let length = results.length
+  let items = length ? results.items : []
 
-    while (length > 0 && results.hasNext()) {
-        results = await results.next();
-        length = results.length;
-        if (length) {
-            items = items.concat(results.items)
-        }
+  while (length > 0 && results.hasNext()) {
+    results = await results.next()
+    length = results.length
+    if (length) {
+      items = items.concat(results.items)
     }
-    return items;
+  }
+  return items
 }
 
-export async function getMultiReferencePropertyFromCollection(multiRefColumn, collection, collectionId) {
-    
-    if (!multiRefColumn) {
-        throw new Error("Missing multiRefColumn");
-    }
-    if (!collection) {
-        throw new Error("Missing collection");
-    }
-    if (!collectionId) {
-        throw new Error("Missing collectionId");
-    }
+export async function getMultiReferencePropertyFromCollection(
+  multiRefColumn,
+  collection,
+  collectionId
+) {
+  if (!multiRefColumn) {
+    throw new Error('Missing multiRefColumn')
+  }
+  if (!collection) {
+    throw new Error('Missing collection')
+  }
+  if (!collectionId) {
+    throw new Error('Missing collectionId')
+  }
 
-    let response = await wixData.queryReferenced(collection,collectionId,multiRefColumn);
-    return response.items;
+  let response = await wixData.queryReferenced(
+    collection,
+    collectionId,
+    multiRefColumn
+  )
+  return response.items
 }
-
 
 export async function getFileSizeInBytes(url) {
-    try {
-        const response = await fetch(url, { method: "HEAD" });
-        
-        const clen = response?.headers?._headers['content-length'][0]
-        console.log(clen);
-        console.log(response);
-        return clen;
-    } catch (err) {
-        throw Error("failed pulling filesize for "+url)
-    }
+  try {
+    const response = await fetch(url, { method: 'HEAD' })
+
+    const clen = response?.headers?._headers['content-length'][0]
+    console.log(clen)
+    console.log(response)
+    return clen
+  } catch (err) {
+    throw Error('failed pulling filesize for ' + url)
+  }
 }
