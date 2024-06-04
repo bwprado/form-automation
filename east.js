0b9866338453906e27a1a0da81bce07d11be341a
bruno <bwprado@gmail.com>
2024-06-03 23:12:17 -0300

    Refactor to use code for the events repeater and integrate with Special Events.

--- east.js
+++ east.js
@@ -1,174 +1,103 @@
-// For full API documentation, including code examples, visit https://wix.to/94BuAAs
-import wixSite from 'wix-site';
-import wixData from 'wix-data';
-import wixLocation from 'wix-location';
+import wixSite from 'wix-site'
+import wixLocation from 'wix-location'
 
-$w.onReady(function () {
-    // Prefetch
-    let response = wixSite.prefetchPageResources({
-        "pages": ["/boulevard-kids-east", "/campus-west-murfreesboro", "/new-home"]
-    });
+import { Campuses } from 'public/types'
+import { format } from 'date-fns'
+import { getEvents, getSpecialEvents, parseSpecialEvents } from 'public/data'
 
-    if (response.errors) {
-        // handle errors
+/**
+ * @typedef {import('public/types').Event} Event
+ */
+
+/**
+ * @function prepareRepeaterEvents
+ * @param {any} $item
+ * @param {Event} itemData
+ */
+async function prepareRepeaterEvents($item, itemData) {
+  $item('#buttonEvent').link = itemData['link-events-eventTitle']
+  $item('#imageEvent').src = itemData.eventImageLandscape
+  $item('#textEventName').text = itemData.eventTitle
+  $item('#textDate').text = itemData?.eventEndDate
+    ? format(itemData.eventEndDate, 'MMM d, yyyy')
+    : '-'
+  $item('#textTime').text = itemData?.eventEndDate
+    ? format(itemData.eventEndDate, 'h:mm a')
+    : '-'
+}
+
+$w.onReady(async function () {
+  $w('#repeaterEvents').onItemReady(prepareRepeaterEvents)
+
+  const eventsQuery = await getEvents({
+    start: new Date(),
+    campuses: [Campuses.East]
+  })
+  const specialEventsQuery = await getSpecialEvents({
+    date: new Date(),
+    campuses: [Campuses.East]
+  })
+
+  const allEvents = [
+    ...eventsQuery.items,
+    ...parseSpecialEvents(specialEventsQuery.items)
+  ].sort((a, b) => a.eventEndDate - b.eventEndDate)
+
+  $w('#repeaterEvents').data = allEvents
+
+  $w('#sectionEvents')[allEvents.length ? 'expand' : 'collapse']()
+  $w('#noResText')[allEvents.length ? 'hide' : 'show']()
+
+  wixSite.prefetchPageResources({
+    pages: ['/boulevard-kids-east', '/campus-west-murfreesboro', '/new-home']
+  })
+
+  $w('#dropdownCampus').onChange((event) => {
+    let dropdownurl = $w('#dropdownCampus').value
+    wixLocation.to(dropdownurl)
+  })
+
+  // Staff emails
+  $w('#repeater5Staff').onItemReady(($item, itemData, index) => {
+    $item('#button5Contact').link =
+      'mailto:' + itemData.staffEmail + '?subject=East Campus'
+    // Staff Contact Button
+    if (itemData.staffEmail) {
+      $item('#button5Contact').show()
+    } else {
+      $item('#button5Contact').hide()
     }
-
-    // Dropdown Navigation
-    $w("#dropdownCampus").onChange((event) => {
-        let dropdownurl = $w('#dropdownCampus').value;
-        wixLocation.to(dropdownurl);
-    });
-
-    // Staff emails
-    $w("#repeater5Staff").onItemReady(($item, itemData, index) => {
-        $item("#button5Contact").link = "mailto:" + itemData.staffEmail + "?subject=East Campus";
-        // Staff Contact Button
-        if (itemData.staffEmail) {
-            $item("#button5Contact").show();
-        } else {
-            $item("#button5Contact").hide();
-        }
-    });
-
-    //filter past Event Dates & Campus
-    var today = new Date();
-    $w('#datasetEvents').setFilter(wixData.filter()
-            .ge('eventStartDate', today)
-            .hasSome('eventAssociatedCampuses', ["9cf1b820-8a76-436d-b61c-c26346e98da5"])
-            .ne('eventIsHidden', true)
-            .ne('ministrySpecificEvent', true)
-
-        )
-        .then(() => {
-            // No Events Message
-            errorTextResult();
-
-        });
-
-    // No Event Repeater Results
-    function errorTextResult() {
-        $w("#datasetEvents").onReady(() => {
-            let count = $w("#datasetEvents").getTotalCount();
-
-            if (count > 0) {
-                $w('#sectionEvents').expand();
-                $w('#noResText').hide();
-            }
-            if (count === 0) {
-                $w('#sectionEvents').collapse();
-                $w('#noResText').show();
-            }
-        });
-    }
-
-/*
-    $w("#repeaterEvents").onItemReady(async ($item, itemData, index) => {
-        let redirectUrl = itemData.redirectUrl;
-
-        // redirect from dynamic event page to another page
-        if (redirectUrl) {
-            $item("#buttonEvent").link = redirectUrl;
-            $item("#buttonEvent").target = "_self";
-        } else {
-            $item("#buttonEvent").link;
-        }
-
-    });
-*/
-});
-
+  })
+})
 // What to Expect
 export function buttonWorship_click(event) {
-    $w('#buttonWorship').disable();
-    $w('#buttonChildren').enable();
-    $w('#buttonYouth').enable();
-    $w('#buttonAdults').enable();
-    $w('#multiStateWhat').changeState("stateA");
+  $w('#buttonWorship').disable()
+  $w('#buttonChildren').enable()
+  $w('#buttonYouth').enable()
+  $w('#buttonAdults').enable()
+  $w('#multiStateWhat').changeState('stateA')
 }
 
 export function buttonChildren_click(event) {
-    $w('#buttonWorship').enable();
-    $w('#buttonChildren').disable();
-    $w('#buttonYouth').enable();
-    $w('#buttonAdults').enable();
-    $w('#multiStateWhat').changeState("stateB");
+  $w('#buttonWorship').enable()
+  $w('#buttonChildren').disable()
+  $w('#buttonYouth').enable()
+  $w('#buttonAdults').enable()
+  $w('#multiStateWhat').changeState('stateB')
 }
 
 export function buttonYouth_click(event) {
-    $w('#buttonWorship').enable();
-    $w('#buttonChildren').enable();
-    $w('#buttonYouth').disable();
-    $w('#buttonAdults').enable();
-    $w('#multiStateWhat').changeState("stateC");
+  $w('#buttonWorship').enable()
+  $w('#buttonChildren').enable()
+  $w('#buttonYouth').disable()
+  $w('#buttonAdults').enable()
+  $w('#multiStateWhat').changeState('stateC')
 }
 
 export function buttonAdults_click(event) {
-    $w('#buttonWorship').enable();
-    $w('#buttonChildren').enable();
-    $w('#buttonYouth').enable();
-    $w('#buttonAdults').disable();
-    $w('#multiStateWhat').changeState("stateD");
+  $w('#buttonWorship').enable()
+  $w('#buttonChildren').enable()
+  $w('#buttonYouth').enable()
+  $w('#buttonAdults').disable()
+  $w('#multiStateWhat').changeState('stateD')
 }
-
-// What to Expect OLD
-/*
-export function buttonWorship_click(event) {
-    $w('#buttonWorship').hide();
-    $w('#buttonWorshipSelected').show();
-    $w('#buttonChildren').show();
-    $w('#buttonChildrenSelected').hide();
-    $w('#buttonYouth').show();
-    $w('#buttonYouthSelected').hide();
-    $w('#buttonAdults').show();
-    $w('#buttonAdultsSelected').hide();
-    $w('#boxWorship').expand();
-    $w('#boxChildren').collapse();
-    $w('#boxYouth').collapse();
-    $w('#boxAdults').collapse();
-}
-
-export function buttonChildren_click(event) {
-    $w('#buttonWorship').show();
-    $w('#buttonWorshipSelected').hide();
-    $w('#buttonChildren').hide();
-    $w('#buttonChildrenSelected').show();
-    $w('#buttonYouth').show();
-    $w('#buttonYouthSelected').hide();
-    $w('#buttonAdults').show();
-    $w('#buttonAdultsSelected').hide();
-    $w('#boxWorship').collapse();
-    $w('#boxChildren').expand();
-    $w('#boxYouth').collapse();
-    $w('#boxAdults').collapse();
-}
-
-export function buttonYouth_click(event) {
-    $w('#buttonWorship').show();
-    $w('#buttonWorshipSelected').hide();
-    $w('#buttonChildren').show();
-    $w('#buttonChildrenSelected').hide();
-    $w('#buttonYouth').hide();
-    $w('#buttonYouthSelected').show();
-    $w('#buttonAdults').show();
-    $w('#buttonAdultsSelected').hide();
-    $w('#boxWorship').collapse();
-    $w('#boxChildren').collapse();
-    $w('#boxYouth').expand();
-    $w('#boxAdults').collapse();
-}
-
-export function buttonAdults_click(event) {
-    $w('#buttonWorship').show();
-    $w('#buttonWorshipSelected').hide();
-    $w('#buttonChildren').show();
-    $w('#buttonChildrenSelected').hide();
-    $w('#buttonYouth').show();
-    $w('#buttonYouthSelected').hide();
-    $w('#buttonAdults').hide();
-    $w('#buttonAdultsSelected').show();
-    $w('#boxWorship').collapse();
-    $w('#boxChildren').collapse();
-    $w('#boxYouth').collapse();
-    $w('#boxAdults').expand();
-}
-*/
