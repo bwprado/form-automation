7f31c95c1df0691d4ac078ad1bbf43cd8097c29c
bruno <bwprado@gmail.com>
2024-06-03 23:12:21 -0300

    Refactor to use code for the events repeater and integrate with Special Events.

--- boulevard-kids.js
+++ boulevard-kids.js
@@ -1,16 +1,78 @@
-import wixData from 'wix-data'
 import wixSite from 'wix-site'
-//import {collapseElementsInRepeater, setElementOpacity, setColorsFullAlpha} from 'public/utils.js'
+
+import { format } from 'date-fns'
+import { getEvents, getSpecialEvents, parseSpecialEvents } from 'public/data'
 import { getMultiReferencePropertyFromCollection } from 'public/dataUtilities.js'
+import { Ministries } from 'public/types'
 
-// Our main onReady event handler we will use asynch as the mode of use so that
-// we can simplify the use of promise based functions.
-$w.onReady(async () => {
-  // Configure handlers
-  // Only create handler once in the onReady scope
+/**
+ * @typedef {import('public/types').Event} Event
+ */
 
-  // Prefetch
-  let response = wixSite.prefetchPageResources({
+/**
+ * @function prepareRepeaterEvents
+ * @param {any} $item
+ * @param {Event} itemData
+ */
+async function prepareRepeaterEvents($item, itemData) {
+  $item('#buttonEvent').link = itemData['link-events-eventTitle']
+  $item('#imageEvent').src = itemData.eventImageLandscape
+  $item('#textEventName').text = itemData.eventTitle
+  $item('#textDate').text = itemData?.eventEndDate
+    ? format(itemData.eventEndDate, 'MMM d, yyyy')
+    : '-'
+  $item('#textTime').text = itemData?.eventEndDate
+    ? format(itemData.eventEndDate, 'h:mm a')
+    : '-'
+
+  let campuses = itemData
+    ? await getMultiReferencePropertyFromCollection(
+        'eventAssociatedCampuses',
+        'Events',
+        itemData._id
+      )
+    : []
+  console.log(
+    `campuses [${campuses.length}] event: [${
+      itemData.eventTitle
+    }] color ${JSON.stringify($item('#tagCampus').style)}`
+  )
+
+  if (campuses.length === 1) {
+    $item('#tagCampus').label = campuses[0].campusFullTitle
+    $item('#tagCampus')
+      .expand()
+      .catch((error) => console.log(`Show Error: ${error.message}`))
+  } else {
+    $item('#tagCampus').label = ''
+    $item('#tagCampus')
+      .collapse()
+      .catch((error) => console.log(`Hide Error: ${error.message}`))
+  }
+}
+
+$w.onReady(async function () {
+  $w('#repeaterEvents').onItemReady(prepareRepeaterEvents)
+
+  const eventsQuery = await getEvents({
+    start: new Date(),
+    ministries: [Ministries.BoulevardKidsEast, Ministries.BoulevardKidsWest]
+  })
+  const specialEventsQuery = await getSpecialEvents({
+    date: new Date()
+  })
+
+  const allEvents = [
+    ...eventsQuery.items,
+    ...parseSpecialEvents(specialEventsQuery.items)
+  ].sort((a, b) => a.eventEndDate - b.eventEndDate)
+
+  $w('#repeaterEvents').data = allEvents
+
+  $w('#sectionEvents')[allEvents.length ? 'expand' : 'collapse']()
+  $w('#noResText')[allEvents.length ? 'hide' : 'show']()
+
+  wixSite.prefetchPageResources({
     pages: [
       '/boulevard-kids-east',
       '/boulevard-kids-online',
@@ -18,67 +80,4 @@ $w.onReady(async () => {
       '/sonshine-school'
     ]
   })
-
-  if (response.errors) {
-    // handle errors
-  }
-
-  // filter past Event Dates & Campus
-  var today = new Date()
-
-  // Use await instead of promise construct as it is easier to work with.
-  await $w('#datasetEvents').setFilter(
-    wixData
-      .filter()
-      .ge('eventStartDate', today)
-      //.eq('ministrySpecificEvent', true)
-      .hasSome('eventMinistries', [
-        'b594a941-1bd9-45b6-859d-9fb59eba3433',
-        '04b632be-e422-4648-afca-be5c2a468b0f'
-      ])
-      .ne('eventIsHidden', true)
-  )
-
-  checkEventFilterResult()
-
-  $w('#repeaterEvents').onItemReady(async ($item, itemData, index) => {
-    let campuses = itemData
-      ? await getMultiReferencePropertyFromCollection(
-          'eventAssociatedCampuses',
-          'Events',
-          itemData._id
-        )
-      : []
-    console.log(
-      `campuses [${campuses.length}] event: [${
-        itemData.eventTitle
-      }] color ${JSON.stringify($item('#tagCampus').style)}`
-    )
-
-    if (campuses.length === 1) {
-      $item('#tagCampus').label = campuses[0].campusFullTitle
-      $item('#tagCampus')
-        .expand()
-        .catch((error) => console.log(`Show Error: ${error.message}`))
-    } else {
-      $item('#tagCampus').label = ''
-      $item('#tagCampus')
-        .collapse()
-        .catch((error) => console.log(`Hide Error: ${error.message}`))
-    }
-  })
-
-  // Checks to see if the Events Repeater filter generated Results
-  // Called after repeater filter has been triggered.
-  function checkEventFilterResult() {
-    let count = $w('#datasetEvents').getTotalCount()
-
-    if (count > 0) {
-      $w('#sectionEvents').expand()
-      $w('#noResText').hide()
-    } else {
-      $w('#sectionEvents').collapse()
-      $w('#noResText').show()
-    }
-  }
 })
