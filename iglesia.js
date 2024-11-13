1b184a1c0708e9a61e7eff335c6c85277c05a50c
Bruno <bwprado@gmail.com>
2024-11-13 09:48:27 -0300

    Owner changes

--- iglesia.js
+++ iglesia.js
@@ -17,11 +17,11 @@ async function prepareRepeaterEvents($item, itemData) {
   $item('#buttonEvent').link = itemData['link-events-eventTitle']
   $item('#imageEvent').src = itemData.eventImageLandscape
   $item('#textEventName').text = itemData.eventTitle
-  $item('#textDate').text = itemData?.eventEndDate
-    ? format(itemData.eventEndDate, 'MMM d, yyyy')
+  $item('#textDate').text = itemData?.eventStartDate
+    ? format(itemData.eventStartDate, 'MMM d, yyyy')
     : '-'
-  $item('#textTime').text = itemData?.eventEndDate
-    ? format(itemData.eventEndDate, 'h:mm a')
+  $item('#textTime').text = itemData?.eventStartDate
+    ? format(itemData.eventStartDate, 'h:mm a')
     : '-'
 }
 
